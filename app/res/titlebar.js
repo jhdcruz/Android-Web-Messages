ec2ee6943f00c2f177a37d2aaf2c743036a6878b
jhdcruz <jhdcruz00@gmail.com>
2019-10-09 15:15:20 +0800

    1.1.1 | Dependencies Update

--- app/res/titlebar.js
+++ app/res/titlebar.js
@@ -1,35 +1,35 @@
-const electronTitlebarWindows = require('electron-titlebar-windows');
-const { remote } = require('electron');
+const electronTitlebarWindows = require("electron-titlebar-windows");
+const { remote } = require("electron");
 /** Options */
 let titlebar = new electronTitlebarWindows({
-    color: 'rgb(230, 230, 230)',
-    backgroundColor: 'rgb(50, 50, 50)',
-    draggable: true,
+  color: "rgb(230, 230, 230)",
+  backgroundColor: "rgb(50, 50, 50)",
+  draggable: true
 });
 /**
  * DOM
  */
 titlebar.appendTo();
 /** Event#close */
-titlebar.on('close', () => {
-    console.info('close');
-    remote.getCurrentWindow().hide();
+titlebar.on("close", () => {
+  console.info("close");
+  remote.getCurrentWindow().hide();
 });
 /** Event#fullscreen */
-titlebar.on('fullscreen', () => {
-    let window = remote.BrowserWindow.getFocusedWindow();
-    window.isMaximized() ? window.unmaximize() : window.maximize();
+titlebar.on("fullscreen", () => {
+  let window = remote.BrowserWindow.getFocusedWindow();
+  window.isMaximized() ? window.unmaximize() : window.maximize();
 });
 /** Event#minimize */
-titlebar.on('minimize', () => {
-    console.info('minimize');
-    remote.getCurrentWindow().minimize();
+titlebar.on("minimize", () => {
+  console.info("minimize");
+  remote.getCurrentWindow().minimize();
 });
 /** Event#maximize */
-titlebar.on('maximize', () => {
-    console.info('maximize');
-    let window = remote.BrowserWindow.getFocusedWindow();
-    window.isMaximized() ? window.unmaximize() : window.maximize();
+titlebar.on("maximize", () => {
+  console.info("maximize");
+  let window = remote.BrowserWindow.getFocusedWindow();
+  window.isMaximized() ? window.unmaximize() : window.maximize();
 });
 
 // source: `https://github.com/sidneys/electron-titlebar-windows/blob/master/app/main.html`
